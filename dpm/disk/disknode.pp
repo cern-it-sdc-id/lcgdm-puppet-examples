4d13f3163f9c8a6775e9c808a0e077188dbb1908
Andrea Manzi <andrea.manzi@cern.ch>
2014-04-03 17:49:52 +0200

    added lfc, haednode and disknode examples

---
+++ dpm/disk/disknode.pp
@@ -0,0 +1,190 @@
+#
+# This is an example configuration for a DPM Disk Node.
+#
+# You can check the puppet module 'lcgdm' and 'dmlite' for any additional options available.
+# !! Please replace the placeholders for usernames and passwords !!
+#
+
+#
+# The standard variables are collected here:
+#
+$headnode_fqdn = "HEADNODE"
+$token_password = "TOKEN_PASSWORD"
+$mysql_root_pass = "PASS"
+$db_user = "dpmmgr"
+$db_pass = "MYSQLPASS"
+$localdomain = "cern.ch"
+$volist = ["dteam", "atlas", "lhcb"]
+$disk_nodes = "${::fqdn} dpmdisk01.cern.ch dpmdisk02.cern.ch"
+$xrootd_sharedkey = "A32TO64CHARACTERKEY"
+$debug = false
+
+Class[Lcgdm::Base::Install] -> Class[Lcgdm::Rfio::Install]
+Class[Dmlite::Plugins::Adapter::Install] ~> Class[Dmlite::Dav::Service]
+Class[Dmlite::Plugins::Adapter::Install] ~> Class[Dmlite::Gridftp]
+
+#
+# The firewall configuration
+#
+firewall{"050 allow http and https":
+  proto  => "tcp",
+  dport  => [80, 443],
+  action => "accept"
+}
+firewall{"050 allow rfio":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "5001",
+  action => "accept"
+}
+firewall{"050 allow rfio range":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "20000-25000",
+  action => "accept"
+}
+firewall{"050 allow gridftp control":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "2811",
+  action => "accept"
+}
+firewall{"050 allow gridftp range":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "20000-25000",
+  action => "accept"
+}
+firewall{"050 allow xrootd":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "1095",
+  action => "accept"
+}
+firewall{"050 allow cmsd":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "1094",
+  action => "accept"
+}
+
+firewall{"050 allow DPNS":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "5010",
+  action => "accept"
+}
+firewall{"050 allow DPM":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "5015",
+  action => "accept"
+}
+
+#
+# lcgdm configuration.
+#
+class{"lcgdm::base::config":}
+class{"lcgdm::base::install":}
+
+class{"lcgdm::ns::client":
+  flavor  => "dpns",
+  dpmhost => "${headnode_fqdn}"
+}
+
+#
+# RFIO configuration.
+#
+class{"lcgdm::rfio":
+  dpmhost => "${headnode_fqdn}",
+}
+
+#
+# Entries in the shift.conf file, you can add in 'host' below the list of
+# machines that the DPM should trust (if any).
+#
+lcgdm::shift::trust_value{
+  "DPM TRUST":
+    component => "DPM",
+    host      => "${headnode_fqdn} ${disk_nodes}";
+  "DPNS TRUST":
+    component => "DPNS",
+    host      => "${headnode_fqdn} ${disk_nodes}";
+  "RFIO TRUST":
+    component => "RFIOD",
+    host      => "${headnode_fqdn} ${disk_nodes}",
+    all       => true
+}
+lcgdm::shift::protocol{"PROTOCOLS":
+  component => "DPM",
+  proto     => "rfio gsiftp http https xroot"
+}
+
+#
+# VOMS configuration (same VOs as above).
+#
+class{"voms::alice":}
+class{"voms::atlas":}
+class{"voms::cms":}
+class{"voms::dteam":}
+class{"voms::lhcb":}
+
+#
+# Gridmapfile configuration.
+#
+$groupmap = {
+  "vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam/Role=lcgadmin"   => "dteam",
+  "vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam/Role=production" => "dteam",
+  "vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam"                 => "dteam",
+  "vomss://voms.cern.ch:8443/voms/atlas?/atlas/Role=lcgadmin"         => "atlas",
+  "vomss://voms.cern.ch:8443/voms/atlas?/atlas/Role=production"       => "atlas",
+  "vomss://voms.cern.ch:8443/voms/atlas?/atlas"                       => "atlas"
+}
+lcgdm::mkgridmap::file {"lcgdm-mkgridmap":
+  configfile   => "/etc/lcgdm-mkgridmap.conf",
+  mapfile      => "/etc/lcgdm-mapfile",
+  localmapfile => "/etc/lcgdm-mapfile-local",
+  logfile      => "/var/log/lcgdm-mkgridmap.log",
+  groupmap     => $groupmap,
+  localmap     => {"nobody" => "nogroup"}
+}
+
+#
+# dmlite plugin configuration.
+class{"dmlite::disk":
+  token_password => "${token_password}",
+  dpmhost        => "${headnode_fqdn}",
+  nshost         => "${headnode_fqdn}",
+}
+
+#
+# dmlite frontend configuration.
+#
+class{"dmlite::dav":}
+class{"dmlite::gridftp":
+  dpmhost => "${headnode_fqdn}"
+}
+
+# The XrootD configuration is a bit more complicated and
+# the full config (incl. federations) will be explained here:
+# https://svnweb.cern.ch/trac/lcgdm/wiki/Dpm/Xroot/PuppetSetup
+
+#
+# The simplest xrootd configuration.
+#
+class{"xrootd::config":
+  xrootd_user  => 'dpmmgr',
+  xrootd_group => 'dpmmgr'
+}
+$sharedkey = "32TO64CHARACTERSTRING"
+class{"dmlite::xrootd":
+  nodetype              => [ 'disk' ],
+  domain                => "${localdomain}",
+  dpm_xrootd_debug      => $debug,
+  dpm_xrootd_sharedkey  => "${xrootd_sharedkey}"
+}
+
+#
+# dmlite shell configuration.
+#
+#class{"dmlite::shell":}
---
+++ lfc/lfc.pp
@@ -0,0 +1,125 @@
+#
+# This is an example configuration for the LFC service.
+#
+# You can check the puppet modules 'lcgdm' and 'dmlite' for any additional options available.
+# !! Please replace the placeholders for usernames and passwords !!
+#
+
+#
+# The standard variables are collected here:
+#
+$mysql_root_pass = "PASS"
+$db_user = "dpmmgr"
+$db_pass = "MYSQLPASS"
+$localdomain = "cern.ch"
+$volist = ["dteam", "atlas", "lhcb"]
+$debug = false
+
+
+Class[Mysql::Server] -> Class[Lcgdm::Ns::Service]
+Class[Lcgdm::Ns::Service] -> Class[Lcgdm::Ns::Client]
+Lcgdm::Ns::Domain <| |> -> Lcgdm::Ns::Vo <| |>
+Class[Dmlite::Plugins::Adapter::Install] ~> Class[Dmlite::Dav::Lfc]
+
+#
+# The firewall configuration
+#
+firewall{"050 allow http and https":
+  proto  => "tcp",
+  dport  => [80, 443],
+  action => "accept"
+}
+firewall{"050 allow LFC clients":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "5010",
+  action => "accept"
+}
+firewall{"050 allow LFC DLI clients":
+  state  => "NEW",
+  proto  => "tcp",
+  dport  => "8085",
+  action => "accept"
+}
+#
+# MySQL server setup - disable if it is not local
+#
+class{"mysql::server":
+  service_enabled => true,
+  root_password   => "${mysql_root_pass}"
+}
+
+#
+# lcgdm configuration
+#
+class{"lcgdm::base::config":
+  cert    => "lfccert.pem",
+  certkey => "lfckey.pem",
+  user    => "lfcmgr"
+}
+
+#
+# Nameserver client and server configuration.
+#
+class{"lcgdm::ns":
+  flavor   => "lfc",
+  dbflavor => "mysql",
+  dbuser   => "${db_user}",
+  dbpass   => "${db_pass}",
+}
+
+#
+# dmlite plugins configuration.
+#
+class{"dmlite::lfc":
+  dbflavor        => "mysql",
+  dbuser          => "${db_user}",
+  dbpass          => "${db_pass}",
+}
+class{"dmlite::plugins::librarian":}
+
+#
+# Create path for domain and VOs to be enabled.
+#
+lcgdm::ns::domain{"${localdomain}":}
+lcgdm::ns::vo{$volist:
+  domain  => "${localdomain}",
+}
+
+#
+# Frontends based on dmlite.
+#
+class{"dmlite::dav::lfc":}
+
+#
+# VOMS configuration (same VOs as above).
+#
+class{"voms::dteam":}
+class{"voms::atlas":}
+
+#
+# Gridmapfile configuration.
+#
+class{"lcgdm::mkgridmap::install":}
+$groupmap = {
+  "vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam/Role=lcgadmin"   => "dteam",
+  "vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam/Role=production" => "dteam",
+  "vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam"                 => "dteam"
+}
+lcgdm::mkgridmap::file {"lcgdm-mkgridmap":
+  configfile   => "/etc/lcgdm-mkgridmap.conf",
+  mapfile      => "/etc/lcgdm-mapfile",
+  localmapfile => "/etc/lcgdm-mapfile-local",
+  logfile      => "/var/log/lcgdm-mkgridmap.log",
+  groupmap     => $groupmap,
+  localmap     => {"nobody" => "nogroup"}
+}
+
+lcgdm::shift::trust_value{"lfc-localhost":
+  component => "LFC",
+  host      => "*"
+}
+
+#
+# dmlite shell configuration.
+#class{"dmlite::shell":}
