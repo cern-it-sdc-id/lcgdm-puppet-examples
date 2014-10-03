7192aade35f789ac6b525a769daef23e1d3050ea
andrea-manzi <andrea.manzi@gmail.com>
2014-10-03 18:36:38 +0200

    install the dmlite-shell

--- dpm/head/head+disknode.pp
+++ dpm/head/head+disknode.pp
@@ -23,8 +23,7 @@ $local_db = true
 # Set inter-module dependencies
 #
 Class[Lcgdm::Dpm::Service] -> Class[Dmlite::Plugins::Adapter::Install]
-Class[Lcgdm::Ns::Service] -> Class[Lcgdm::Dpm::Service]
-Class[Lcgdm::Dpm::Service] -> Class[Dmlite::Srm::Service]
+Class[Lcgdm::Ns::Config] -> Class[Dmlite::Srm::Service]
 Class[Dmlite::Head] -> Class[Dmlite::Plugins::Adapter::Install]
 Class[Dmlite::Plugins::Adapter::Install] ~> Class[Dmlite::Srm]
 Class[Dmlite::Plugins::Adapter::Install] ~> Class[Dmlite::Gridftp]
