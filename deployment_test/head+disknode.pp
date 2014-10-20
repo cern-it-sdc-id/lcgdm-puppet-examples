1980ea6c124971f4151cbadb6d32ca09d69a5ce8
andrea-manzi <andrea.manzi@gmail.com>
2014-10-20 15:35:42 +0200

    added dep for memcache

--- deployment_test/head+disknode.pp
+++ deployment_test/head+disknode.pp
@@ -36,9 +36,10 @@ Class[Bdii::Install] -> Class[Lcgdm::Bdii::Dpm]
 Class[Lcgdm::Bdii::Dpm] -> Class[Bdii::Service]
 Class[fetchcrl::service] -> Class[Xrootd::Config]
 #memcache deps
-#Class[Dmlite::Plugins::Memcache::Install] ~> Class[Dmlite::Dav::Service]    
-#Class[Dmlite::Plugins::Memcache::Install] ~> Class[Dmlite::Gridftp]
-#Class[Dmlite::Plugins::Memcache::Install] ~> Class[Dmlite::Srm]
+Class[Lcdm::Base::Config] ~> Class[Dmlite::Plugins::Memcache]
+Class[Dmlite::Plugins::Memcache::Install] ~> Class[Dmlite::Dav::Service]    
+Class[Dmlite::Plugins::Memcache::Install] ~> Class[Dmlite::Gridftp]
+Class[Dmlite::Plugins::Memcache::Install] ~> Class[Dmlite::Srm]
 
 #
 # The firewall configuration
