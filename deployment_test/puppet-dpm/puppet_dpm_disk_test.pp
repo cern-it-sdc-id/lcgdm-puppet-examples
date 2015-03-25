fba4efb52b1c14277ec205273e9c6035d9460a32
andrea-manzi <andrea.manzi@gmail.com>
2015-03-25 17:13:21 +0100

    added all puppet-dpm tests

---
+++ deployment_test/puppet-dpm/puppet_dpm_disk_test.pp
@@ -0,0 +1,8 @@
+class{"dpm::disknode":
+   headnode_fqdn => "dpmhead01.cern.ch"
+   localdomain => "cern.ch",
+   token_password => "TOKEN_PASSWORD",
+   xrootd_sharedkey => "A32TO64CHARACTERKEYTESTTESTTESTTEST",
+   site_name => "CERN_DPM_TEST",
+   volist =>[dteam],
+}
